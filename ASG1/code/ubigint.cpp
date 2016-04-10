88f6cf1b844e5e49f5a04f72b2534883a65639c2
Andy Espejo <Andy.Espejo5@gmail.com>
2016-04-09 20:06:32 -0700

    Turned numbers into vector and implemented most of + operator

--- ASG1/code/ubigint.cpp
+++ ASG1/code/ubigint.cpp
@@ -4,21 +4,50 @@
 #include <exception>
 #include <stack>
 #include <stdexcept>
+#include <math.h>
 using namespace std;
 
 #include "ubigint.h"
 #include "debug.h"
 
-ubigint::ubigint (unsigned long that): uvalue (that) {
-   DEBUGF ('~', this << " -> " << uvalue)
-}
 
-ubigint::ubigint (const string& that): uvalue(0) {
-   for (char digit: that) uvalue = uvalue * 10 + digit - '0';
+
+//Creates a vector of the digits
+ubigint::ubigint (const string& that): ubig_value(0) {
+   for (auto itor = that.rbegin(); itor != that.rend(); ++itor){
+	   ubig_value.push_back(*itor);
+   }
 }
 
 ubigint ubigint::operator+ (const ubigint& that) const {
-   return ubigint (uvalue + that.uvalue);
+   ubigint result;
+   int carry = 0;
+   int sum = 0;
+   //Gets size of biggest to make sure all digits are added
+   if(ubig_value.size() > that.ubig_value.size()){
+	   size_t size_of_result = ubig_value.size();
+   }else{
+	   size_t size_of_result = that.ubig_value.size();
+   }
+   //Actual implementation for adding
+   for(size_t i = 0; i < size_of_result; i++){
+	 sum = carry;
+	 if(i < ubig_value.size()){
+		 sum = sum + ubig_value.at(i)
+	 }
+	 if(i < that.ubig_value.size()){
+		 sum = sum + that.ubig_value.at(i);
+	 }
+	 if(sum > 10){
+		 carry = 1;
+		 result.push_back(sum-10);
+	 }else{
+		 result.push_back(sum);
+	 }
+   }
+   //Add code to get rid of leading zeros
+	return result
+
 }
 
 ubigint ubigint::operator- (const ubigint& that) const {
